bd434edd77676bc8a4cc16c13e6a4e025d7215cc
KotlyarovV <vkotl@yandex.ru>
2017-04-24 12:07:57 +0500

    pictures

--- src/main/java/servlets/AllRequestsServlet.java
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -1,11 +1,17 @@
 package servlets;
 
 import templater.PageGenerator;
+
+import javax.print.attribute.URISyntax;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
 
 public class AllRequestsServlet extends HttpServlet {
 
@@ -14,10 +20,23 @@ public class AllRequestsServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
 
+
+        if (!request.getPathInfo().equals("/")) {
+            FileSender.sendFile(request, response, request.getPathInfo());
+        }
+
         response.setContentType("text/html;charset=utf-8");
+
+        System.out.println(request.getPathInfo());
+        System.out.println(request.getRequestURI());
+        System.out.println(this.getServletName());
 
         response.getWriter().println(PageGenerator.instance().getPage("page.html", null));
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
+    public void doPost (HttpServletRequest request,
+                        HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
---
+++ src/main/java/servlets/FileSender.java
@@ -0,0 +1,46 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Created by vitaly on 24.04.17.
+ */
+public class FileSender {
+
+    public static void sendFile (HttpServletRequest request, HttpServletResponse response , String fileName) throws
+            ServletException, IOException {
+        // String fileName = (String) request.getParameter("file");
+
+
+        ServletOutputStream servletOutputStream = null;
+        BufferedInputStream inputStream = null;
+        try {
+            fileName = "templates" +fileName;
+            servletOutputStream = response.getOutputStream();
+            File file = new File(fileName);
+
+            response.addHeader("Content-Disposition", "attachment; filename=" + fileName);
+            response.setContentLength((int) file.length());
+
+            FileInputStream input = new FileInputStream(file);
+            inputStream = new BufferedInputStream(input);
+            int readBytes = 0;
+            while ((readBytes = inputStream.read()) != -1)
+                servletOutputStream.write(readBytes);
+        }   catch (IOException ioe) {
+            throw new ServletException(ioe.getMessage());
+        } finally {
+            if (servletOutputStream != null)
+                servletOutputStream.close();
+            if (inputStream != null)
+                inputStream.close();
+        }
+    }
+}
